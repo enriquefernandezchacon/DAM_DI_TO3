bcfb4d0d910a72b4bc8b5a4a70a3b429151e2a96
Enrique Fernandez <enriquefernandezchacon@gmail.com>
2023-01-19 19:04:33 +0100

    StringExtension

--- DI03_Tarea_Fernandez_Chacon_EnriqueOctavio/Vistas/Clientes/WindowAnadirCliente.xaml.cs
+++ DI03_Tarea_Fernandez_Chacon_EnriqueOctavio/Vistas/Clientes/WindowAnadirCliente.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
+﻿using DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Extensiones;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Vistas.Clientes
 {
@@ -19,10 +10,63 @@ namespace DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Vistas.Clientes
     /// </summary>
     public partial class WindowAnadirCliente : Window
     {
+        private List<string> errores = new List<string>();
         public WindowAnadirCliente()
         {
             InitializeComponent();
         }
+
+        private void AnadirCliente(object sender, RoutedEventArgs e)
+        {
+            ComprobarCampos();
+            
+            
+        }
+
+        private void ComprobarCampos()
+        {
+            if (string.IsNullOrEmpty(TextBoxNombre.Text.Trim()))
+            {
+                errores.Add("nombre".ErrorVacio());
+            }
+            else if (!TextBoxNombre.Text.EsValido()) 
+            {
+                errores.Add("nombre".ErrorFormato());
+            }
+
+            if (string.IsNullOrEmpty(TextBoxApellidos.Text.Trim()))
+            {
+                errores.Add("apellidos".ErrorVacio());
+            }
+            else if (!TextBoxApellidos.Text.EsValido())
+            {
+                errores.Add("apellidos".ErrorFormato());
+            }
+
+            //TELEFONO
+
+            if (string.IsNullOrEmpty(TextBoxTelefono.Text.Trim()))
+            {
+                errores.Add("telefono".ErrorVacio());
+            }
+            else if (!TextBoxTelefono.Text.EsTelefono())
+            {
+                errores.Add("telefono".ErrorFormato());
+            }
+            //EMAIL
+            if (string.IsNullOrEmpty(TextBoxTelefono.Text.Trim()))
+            {
+                errores.Add("email".ErrorVacio());
+            }
+            else if (!TextBoxTelefono.Text.EsEmail())
+            {
+                errores.Add("email".ErrorFormato());
+            }
+            //DNI
+            //DIRECCION
+        }
+
+        
 
         private void Window_Closed(object sender, EventArgs e)
         {
