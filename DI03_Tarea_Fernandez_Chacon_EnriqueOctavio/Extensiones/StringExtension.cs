ae8524b4567972dcd369f0cd6694166c3f43bbf3
Enrique Fernandez <enriquefernandezchacon@gmail.com>
2023-01-20 13:07:32 +0100

    Añadidos Mensajes De Inforamción

--- DI03_Tarea_Fernandez_Chacon_EnriqueOctavio/Extensiones/StringExtension.cs
+++ DI03_Tarea_Fernandez_Chacon_EnriqueOctavio/Extensiones/StringExtension.cs
@@ -6,6 +6,7 @@ namespace DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Extensiones
     public static class StringExtension
     {
         private static readonly Regex REGEX_TELEFONO = new Regex(@"[6789]\d{8}");
+        private static readonly Regex REGEX_DNI= new Regex(@"\d{8}[A-Z]");
         private static readonly Regex REGEX_EMAIL = new Regex(@"[A-z0-9._%+-]+@[A-z0-9.-]+[A-z0-9]+\.[A-z]{2,3}$");
         public static bool EsValido(this string s)
         {
@@ -20,6 +21,11 @@ namespace DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Extensiones
         public static bool EsEmail(this string email)
         {
             return REGEX_EMAIL.IsMatch(email.Trim());
+        }
+
+        public static bool EsDni(this string dni)
+        {
+            return REGEX_DNI.IsMatch(dni.Trim());
         }
 
         public static string ErrorVacio(this string nombreCampo)
--- DI03_Tarea_Fernandez_Chacon_EnriqueOctavio/Vistas/Clientes/WindowAnadirCliente.xaml.cs
+++ DI03_Tarea_Fernandez_Chacon_EnriqueOctavio/Vistas/Clientes/WindowAnadirCliente.xaml.cs
@@ -1,6 +1,10 @@
-﻿using DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Extensiones;
+﻿using DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Componentes;
+using DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Extensiones;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Vistas.Clientes
@@ -18,13 +22,49 @@ namespace DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Vistas.Clientes
 
         private void AnadirCliente(object sender, RoutedEventArgs e)
         {
+            errores = new();
             ComprobarCampos();
-            
-            
+            if (errores.Count > 0)
+            {
+                MostrarErrores();
+            } else
+            {
+                ProcesarPeticion();
+            }
+        }
+
+        private void MostrarErrores()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("El formulario presente los siguientes errores:");
+            sb.AppendLine();
+            foreach (var error in errores)
+            {
+                sb.AppendLine(string.Concat("- ", error));
+            }
+            sb.AppendLine();
+            sb.AppendLine("Revise los errores y vuelva a intentarlo");
+
+            MessageBox.Show(sb.ToString(), "Errores en el formulario", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void ProcesarPeticion()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Cliente añadido con éxito.");
+            sb.AppendLine();
+            sb.AppendLine(string.Concat(Regex.Replace(TextBoxNombre.Text.Trim(), @"\s+", " "), " ", Regex.Replace(TextBoxApellidos.Text.Trim(), @"\s+", " ")));
+            sb.AppendLine(string.Concat("Fecha de nacimiento: ", DatePickerFechaNacimiento.ToString().Substring(0,10)));
+            sb.AppendLine(string.Concat("Teléfono: ", TextBoxTelefono.Text));
+            sb.AppendLine(string.Concat("Email: ", TextBoxMail.Text));
+            sb.AppendLine(string.Concat("Dirección: ", TextBoxDireccion.Text));
+
+            MessageBox.Show(sb.ToString(), "Cliente Añadido", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void ComprobarCampos()
         {
+            // NOMBRE
             if (string.IsNullOrEmpty(TextBoxNombre.Text.Trim()))
             {
                 errores.Add("nombre".ErrorVacio());
@@ -34,6 +74,7 @@ namespace DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Vistas.Clientes
                 errores.Add("nombre".ErrorFormato());
             }
 
+            //APELLIDOS
             if (string.IsNullOrEmpty(TextBoxApellidos.Text.Trim()))
             {
                 errores.Add("apellidos".ErrorVacio());
@@ -43,8 +84,7 @@ namespace DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Vistas.Clientes
                 errores.Add("apellidos".ErrorFormato());
             }
 
-            //TELEFONO
-
+            //TELEFON
             if (string.IsNullOrEmpty(TextBoxTelefono.Text.Trim()))
             {
                 errores.Add("telefono".ErrorVacio());
@@ -53,17 +93,39 @@ namespace DI03_Tarea_Fernandez_Chacon_EnriqueOctavio.Vistas.Clientes
             {
                 errores.Add("telefono".ErrorFormato());
             }
+
             //EMAIL
-            if (string.IsNullOrEmpty(TextBoxTelefono.Text.Trim()))
+            if (string.IsNullOrEmpty(TextBoxMail.Text.Trim()))
             {
                 errores.Add("email".ErrorVacio());
             }
-            else if (!TextBoxTelefono.Text.EsEmail())
+            else if (!TextBoxMail.Text.EsEmail())
             {
                 errores.Add("email".ErrorFormato());
             }
+
             //DNI
+            if (string.IsNullOrEmpty(CustomDni.Dni.Trim()))
+            {
+                errores.Add("dni".ErrorVacio());
+            }
+            else if (!CustomDni.Dni.EsDni())
+            {
+                errores.Add("dni".ErrorFormato());
+            }
+
             //DIRECCION
+            if (string.IsNullOrEmpty(TextBoxDireccion.Text.Trim()))
+            {
+                errores.Add("dirección".ErrorVacio());
+            }
+
+            //FECHA DE NACIMIENTO
+
+            if (string.IsNullOrEmpty(DatePickerFechaNacimiento.ToString()))
+            {
+                errores.Add("fecha de nacimiento".ErrorVacio());
+            }
         }
 
         
